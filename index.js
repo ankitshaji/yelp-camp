f49a513a79259e75b8374917ebad5ceabf8e7acc
Ankit Shaji <ankitshaji@gmail.com>
2022-08-20 05:21:09 +1200

    nodeJS runtime app connects to mogod + creates db

--- index.js
+++ index.js
@@ -4,6 +4,37 @@
 const express = require("express"); //functionObject //express module
 const path = require("path"); //pathObject //path module
 const app = express(); //appObject
+//mongoose ODM - has callback but also supports promises-ie returns promiseObject (pending,undefined) to -resove(value)(fullfulled,value) or reject(errorMessage)(rejected,errorMessage)
+const mongoose = require("mongoose"); //mongooseObject //mongoose module
+
+// ********************************************************************************
+// CONNECT - nodeJS runtime app connects to default mogod server port + creates db
+// ********************************************************************************
+//async(ie continues running outside code if it hits an await inside) named function expression
+//implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
+//async function expression without an await is just a normal syncronous function expression
+async function main() {
+  try {
+    //mongooseObject.method(url/defaultPortNo/databaseToUse) //returns promiseObject pending
+    await mongoose.connect("mongodb://localhost:27017/yelp-camp-db");
+    //promisObject resolved
+    console.log("Database Connected");
+  } catch (err) {
+    //promisObject rejected
+    //catches any initial error that occure when establising connection
+    console.log("Mongo intial connection error has occured");
+    console.log(err);
+  }
+}
+main(); //execute async named function expression
+//Dont need to wait for promiseObject to resolve - Operation Buffering
+//mongoose lets us use models immediately after,without waiting for mongoose to eastablish a connection to MongoDB
+
+//catches any error after initial connection
+//mongooseObject.property
+const db = mongoose.connection; //connectionObject
+//connectionObject.method(string,callback)
+db.on("error", console.error.bind(console, "connection error:"));
 
 //when view engine is used express assumes our views ie ejs templates
 //exist in a (default)views directory
