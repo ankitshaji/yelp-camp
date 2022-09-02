ed4f6f202cafbd4f7648c49f4c405230bea2320e
Ankit Shaji <ankitshaji@gmail.com>
2022-09-03 07:04:33 +1200

    DELETE-(DELETE),(/campgrounds/:id/reviews/:reviewId),name-destroy

--- index.js
+++ index.js
@@ -288,7 +288,7 @@ app.get(
     // ***********************************************************
     //READ - querying a collection(campgrounds) for a document by id
     // ***********************************************************
-    //campgroundClassObject.method(idString) ie modelClassObject.method() - same as - db.campgrounds.findOne({_id:"12345"})
+    //campgroundClassObject.method(idString) ie modelClassObject.method() - same as - db.campgrounds.findOne({_id:ObjectId("12345")})
     //returns thenableObject - pending to resolved(dataObject),rejected(errorObject)
     //implicitly throws new Error("messageFromMongoose") - invalid ObjectId format/length
     const foundCampground = await CampgroundClassObject.findById(id); //foundCampground = dataObject ie single first matching jsObject(document)
@@ -323,22 +323,22 @@ app.put(
     //UPDATE - querying a collection(campgrounds) for a document by id then updating it + new key:value pairs neglected
     // **************************************************************************************************************
     //modelClass
-    //campgroundClassObject.method(idString,updateObject,optionObject) ie modelClassObject.method() - same as - db.campgrounds.findOneAndUpdate(({_id:"12345"},{$set:{name:"x",...}},{returnNewDocument:true})
+    //campgroundClassObject.method(idString,updateObject,optionObject) ie modelClassObject.method() - same as - db.campgrounds.findOneAndUpdate(({_id:ObjectId("12345")},{$set:{name:"x",...}},{returnNewDocument:true})
     //returns thenableObject - pending to resolved(dataObject),rejected(errorObject) ie(breaking validation/contraints)
     //To run validations/contraints when updating we need to set runValidators(key) in optionsObject
     //To get the jsObject(document) after update, we need to set new(key) in optionsObject
     //queries (campgrounds)collection of (yelp-camp-db)db for single document by idString and updates/replaces the document with new updateObject(document)
     //implicitly throws new Error("messageFromMongoose") - invalid ObjectId format/length or break validation constraints
-    const foundCampground = await CampgroundClassObject.findByIdAndUpdate(
+    const updatedCampground = await CampgroundClassObject.findByIdAndUpdate(
       id,
       { ...req.body.campground }, //spreading properties into another object //form data/req.body is jsObject //{groupKey:{key/name:inputValue,key/name:inputValue}}
       {
         runValidators: true,
         new: true,
       }
-    ); //foundCampground = dataObject ie single first matching jsObject(document) after it was updated
+    ); //updatedCampground = dataObject ie single first matching jsObject(document) after it was updated
     //fix for page refresh sending duplicate (http structured) PUT request -
-    res.redirect(`/campgrounds/${foundCampground._id}`);
+    res.redirect(`/campgrounds/${updatedCampground._id}`);
     //responseObject.redirect("showPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds/:id
     //responseObject.redirect("showPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
     //thus ending request-response cycle
@@ -352,7 +352,7 @@ app.put(
 //app.method(pathString ,createMiddlewareCallback(async handlerMiddlewareCallback)) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
 //execute handlerMiddlwareCallback if (http structured) DELETE request arrives at path /campgrounds/:id
 //arguments passed in to handlerMiddlewareCallback -
-//-already converted (http structured) request to req jsObject - previous middlewareCallback sets req.method from POST to PUT and called nextCallback
+//-already converted (http structured) request to req jsObject - previous middlewareCallback sets req.method from POST to DELETE and called nextCallback
 //-if not already created create res jsObject
 //-nextCallback
 //async(ie continues running outside code if it hits an await inside) handlerMiddlwareCallback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
@@ -366,7 +366,7 @@ app.delete(
     //DELETE - querying a collection(campgrounds) for a document by id then deleting it
     // ******************************************************************************
     //modelClass
-    //campgroundClassObject.method(idString) ie modelClassObject.method() - same as - db.campgrounds.findOneAndDelete(({_id:"12345"})
+    //campgroundClassObject.method(idString) ie modelClassObject.method() - same as - db.campgrounds.findOneAndDelete(({_id:ObjectId("12345")})
     //returns thenableObject - pending to resolved(dataObject),rejected(errorObject)
     //queries (campgrounds)collection of (yelp-camp-db)db for single document by idString and deletes the document
     //implicitly throws new Error("messageFromMongoose") - invalid ObjectId format/length or break validation constraints
@@ -380,9 +380,9 @@ app.delete(
   })
 );
 
-//****************************************************************
-//FARMS ROUTES using NESTED ROUTING - used to send campground_id
-//****************************************************************
+//********************************************************************************************************
+//FARMS ROUTES using NESTED ROUTING - more than one dynamic variable in path - used to send campground_id
+//********************************************************************************************************
 
 //route1
 //httpMethod=POST,path/resource-/campgrounds/:id/reviews  -(pattern match) //:id is a path variable
@@ -430,6 +430,58 @@ app.post(
     //implicitly throws new Error("messageFromMongoose") - break validation contraints
     const updatedCampground = await foundCampground.save(); //updatedCampground = dataObject ie updated jsObject(document)
     //fix for page refresh sending duplicate (http structured) POST request -
+    res.redirect(`/campgrounds/${updatedCampground._id}`);
+    //responseObject.redirect("showPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds/:id
+    //responseObject.redirect("showPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
+    //thus ending request-response cycle
+    //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds/:id
+  })
+);
+
+//route2
+//httpMethod=DELETE,path/resource-/campgrounds/:id/reviews/:reviewId  -(pattern match) //:id and :reviewId are path variables
+//(DELETE) name-destroy,purpose-delete single specific document in (reviews)collection of (yelp-camp-db)db
+//app.method(pathString ,createMiddlewareCallback(async handlerMiddlewareCallback)) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
+//execute handlerMiddlwareCallback if (http structured) DELETE request arrives at path /campgrounds/:id/reviews/:reviewId
+//arguments passed in to handlerMiddlewareCallback -
+//-already converted (http structured) request to req jsObject - previous middlewareCallback sets req.method from POST to DELETE and called nextCallback
+//-if not already created create res jsObject
+//-nextCallback
+//async(ie continues running outside code if it hits an await inside) handlerMiddlwareCallback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
+//async function expression without an await is just a normal syncronous function expression
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    //object keys to variable - Object destructuring
+    const { id, reviewId } = req.params; //pathVariablesObject
+    // ****************************************************************************************************************************************************************************************
+    //UPDATE - querying a collection(campgrounds) for a document by id then updating it - ie deleting reviewModeInstance id reference in campgroundModelInstances reviews arrayObject property
+    // ****************************************************************************************************************************************************************************************
+    //modelClass
+    //campgroundClassObject.method(idString,updateObject,optionObject) ie modelClassObject.method() - same as - db.campgrounds.findOneAndUpdate(({_id:ObjectId("12345")},{$pull:{reviews:ObjectId("123123"),...}},{returnNewDocument:true})
+    //returns thenableObject - pending to resolved(dataObject),rejected(errorObject) ie(breaking validation/contraints)
+    //To run validations/contraints when updating we need to set runValidators(key) in optionsObject
+    //To get the jsObject(document) after update, we need to set new(key) in optionsObject
+    //queries (campgrounds)collection of (yelp-camp-db)db for single document by idString and updates/replaces the document with updateObject(document)
+    //updateObject contains updateOperator - $pull - removes from existing reviews array all instances of reviewId
+    //implicitly throws new Error("messageFromMongoose") - invalid ObjectId format/length or break validation constraints
+    const updatedCampground = await CampgroundClassObject.findByIdAndUpdate(
+      id,
+      {
+        $pull: { reviews: reviewId },
+      },
+      { runValidators: true, new: true }
+    ); //updatedCampground = dataObject ie single first matching jsObject(document) after it was updated
+    // ******************************************************************************
+    //DELETE - querying a collection(reviews) for a document by id then deleting it
+    // ******************************************************************************
+    //modelClass
+    //ReviewClassObject.method(idString) ie modelClassObject.method() - same as - db.reviews.findOneAndDelete({_id:ObjectId("12345")})
+    //returns thenableObject - pending to resolved(dataObject),rejected(errorObject)
+    //queries (reviews)collection of (yelp-camp-db)db for single document by idString and deletes the document
+    //implicitly throws new Error("messageFromMongoose") - invalid ObjectId format/length or break validation constraints
+    const deletedReview = await ReviewClassObject.findByIdAndDelete(reviewId); //deletedReview = dataObject ie single first matching jsObject(document) that was deleted
+    //fix for page refresh sending duplicate (http structured) DELETE request -
     res.redirect(`/campgrounds/${updatedCampground._id}`);
     //responseObject.redirect("showPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds/:id
     //responseObject.redirect("showPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
