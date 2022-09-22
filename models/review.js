a3c134bda095fb1f7422331d21a1a806af1ca8fc
Ankit Shaji <ankitshaji@gmail.com>
2022-09-23 10:45:57 +1200

    associating User model to Review model

--- models/review.js
+++ models/review.js
@@ -5,9 +5,11 @@ const mongoose = require("mongoose"); //mongooseObject //mongoose module
 const { Schema: SchemaClassObject } = mongoose; //mongoose.Schema = SchemaClassObject
 //dont need to connect nodejs runtime app to mongod server port since we are going to export model to where its already connected
 
-//*********************************************************************************
-//CHILD MODEL - ReviewClassObject ie(Model) - represents the (reviews) collection
-//*********************************************************************************
+//************************************************************************************************************************************************
+//CHILD/CHILD MODEL - ReviewClassObject ie(Model) - represents the (reviews) collection - mongoddb(nosql) relationdships(one to billions)
+//each author property in all document in child collection(reviews) contains id references to parent collection(users)
+//************************************************************************************************************************************************
+
 //blueprint of a single document in reviews collection -
 //mongooseObject.schemaMethod = schemaClassObject(objectArgument passed to constructor method)
 //objectArgument-{key:nodejs value type} for collection {keys:value}
@@ -15,9 +17,12 @@ const { Schema: SchemaClassObject } = mongoose; //mongoose.Schema = SchemaClassO
 //setting validtaions/constraints in object - shorthand vs longhand - [string] vs [{properties}] and String vs {type:String,required:true}
 //none
 //mongoose treats [{properties}] object as an another/embedded schemaInstanceObject - (we can prevent id creation)
+//author property is of type:objectID - JS dosn't have that type so get from mongoose
+//ref option - tells mongoose which model to use when populating objectIDs
 const reviewSchemaInstanceObject = new SchemaClassObject({
   body: String,
   rating: Number,
+  author: { type: SchemaClassObject.Types.ObjectId, ref: "User" },
 });
 
 //creating ReviewClassObject ie(Model) - represents a collection (reviews)
