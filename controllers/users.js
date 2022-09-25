2e82e88b1ca92a2798d4faa7d5ac22a339dab7da
Ankit Shaji <ankitshaji@gmail.com>
2022-09-25 22:05:32 +1300

    usersControllerObject - code separation of users routes hMC

---
+++ controllers/users.js
@@ -0,0 +1,122 @@
+//user created module file - can contain functionObjects,variable,classObjects etc which we can export
+const UserClassObject = require("../models/user"); //UserClassObject(ie Model) //self created module/file needs "./" //going back a directory ..
+
+//(named handlerMiddlewareCallback)
+//use in specific routes ie specific method and specific path
+//create method renderRegisterForm on exportObject
+module.exports.renderRegisterForm = (req, res) => {
+  res.render("users/register");
+  //render(ejs filePath) - executes js - converts  ejs file into pure html
+  //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
+  //responseObject.render() - converts and sends res jsObject as (http structure)response //content-type:text/html
+  //thus ending request-response cycle
+};
+
+//(named handlerMiddlewareCallback)
+//use in specific routes ie specific method and specific path
+//create method createUser on exportObject
+module.exports.createUser = async (req, res) => {
+  try {
+    //object keys to variable - Object destructuring
+    const { email, username, password } = req.body; //form data/req.body is jsObject //{key/name:inputValue,key/name:inputValue}}
+    // ***************************************************************************************
+    //CREATE - creating a single new document in the (users) collection of (yelp-camp-db)db
+    // ***************************************************************************************
+    //modelClass
+    //UserClassObject(objectArgument-passed to constructor method)
+    //objectArgument- jsObject{key:value} ie the new document that abides to collectionSchemaInstanceObject
+    //objectArgument has validations/contraints set by collectionSchemaInstanceObject
+    //validations/contraints -
+    //email cannot be empty + other validations/constraints set by passport-local-mongoose for its fields
+    //create modelInstanceObject(ie document) - with new keyword and UserClassObject constructor method
+    const newUser = new UserClassObject({ email: email, username: username });
+    //UserClassObject.customStaticMethod(newUser,"passwordString")
+    //customStaticMethod on UserClassObject - async register()
+    //creates salt property on newUser and adds random saltValue
+    //creates hash property on newUser and adds hashValue - hashValue created by adding random saltValue to passwordString then hashing SaltedPasswordString using Pbkdf2 hashFunction
+    //calls async mongoosemMethod save on newUser - creating users collection in yelp-camp-db and or adding newUser document into users collection - can implicitly throw new Error("messageFromMongoose") - if validation/contraints broken
+    //if async mongooseMethod implicity throws Error("messageFromMongoose") inside async customStaticMethod , it catches that errorInstanceObject and throws its own new Error("messageFromPassportLocalMongoose")
+    //async customStaticMethod returns the savedUser
+    const savedUser = await UserClassObject.register(newUser, password); //savedUser = dataObject ie created jsObject(document)
+    //reqObject.passportObjectMethod(savedUser,callback) //returns promiseObject
+    //passportObjectMethod execution adds the req.user to the current sessionObject + clears previous existing current sessionObject properties - could set keepSessionInfo:true in req.login() to prevent this
+    //ie.passportObjectMethod serializes the savedUser into one value and stores it into temporary data store , making savedUser retrivable through deserializing the one value into req.user
+    //it executes the callback with parameter errorInstanceObject if it occured ,else execute callback with empty parameter
+    //callback sets success flash message and redirects to /campgrounds
+    req.login(savedUser, (err) => {
+      if (err) return next(err); //return to not run rest of code - next(e) passes erroInstanceObject to next customErrorHandlerMiddlewareCallback
+      req.flash("success", "Welcome to Yelp Camp!"); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
+      //fix for page refresh sending duplicate (http structured) POST request -
+      res.redirect("/campgrounds");
+      //responseObject.redirect("indexPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds
+      //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
+      //responseObject.redirect("indexPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
+      //thus ending request-response cycle
+      //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds
+    });
+  } catch (e) {
+    //(errorInstanceObject)
+    //we catch the implicitly thrown new Error("messageFromPassportLocalMongoose") early - closest catch - therefore .catch does not run - we dont pass it in next(e) to customErrorHandlerMiddlewareCallback
+    req.flash("error", e.message); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
+    res.redirect("/register");
+    //responseObject.redirect("registerPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /register
+    //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
+    //responseObject.redirect("registerPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
+    //thus ending request-response cycle
+    //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/register
+  }
+};
+
+//(named handlerMiddlewareCallback)
+//use in specific routes ie specific method and specific path
+//create method renderLoginForm on exportObject
+module.exports.renderLoginForm = (req, res) => {
+  res.render("users/login");
+  //render(ejs filePath) - executes js - converts  ejs file into pure html
+  //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
+  //responseObject.render() - converts and sends res jsObject as (http structure)response //content-type:text/html
+  //thus ending request-response cycle
+};
+
+//(named handlerMiddlewareCallback)
+//use in specific routes ie specific method and specific path
+//create method authenticatedUserRedirect on exportObject
+module.exports.authenticatedUserRedirect = (req, res) => {
+  req.flash("success", "Welcome back!"); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
+  //req.session.returnUrl retrives urlStringObject from returnUrl property of current sessionObject - requires keepSessionInfo:true in optionsObject - else req.login() implicity execution clears previous existing current sessionObject properties
+  //req.session.returnUrl is undefined if directly coming from /login GET form route
+  //undefined(falsy) or stringObject(truthy) / urlStringObject(truthy) or stringObject(dont check)
+  const returnUrl = req.session.returnUrl || "/campgrounds";
+  //deletes the returnUrl property of current sessionObject
+  delete req.session.returnUrl;
+  //fix for page refresh sending duplicate (http structured) POST request -
+  res.redirect(returnUrl);
+  //responseObject.redirect("indexPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds
+  //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
+  //responseObject.redirect("indexPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
+  //thus ending request-response cycle
+  //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds
+};
+
+//(named handlerMiddlewareCallback)
+//use in specific routes ie specific method and specific path
+//create method logoutUser on exportObject
+module.exports.logoutUser = (req, res) => {
+  //reqObject.passportObjectMethod(callback)
+  //passportObjectMethod execution removes the req.user from the current sessionObject + clears previous existing current sessionObject properties - could set keepSessionInfo:true in req.logout() to prevent this
+  //ie.foundUser is retrivable through deserializing the one value from temporary data store into req.user,meaning foundUser was serialized into one value and stored into temporary data store after verifyCallback passed in customAuthenticationMiddlewareCallback at one point
+  //it executes the callback with parameter errorInstanceObject if it occured else execute callback with empty parameter
+  //callback sets success flash message and redirects to /campgrounds
+  req.logout(function (err) {
+    if (err) {
+      return next(err); //return to not run rest of code - next(e) passes erroInstanceObject to next customErrorHandlerMiddlewareCallback
+    }
+    req.flash("success", "Successfully logged out"); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
+    //fix for page refresh sending duplicate (http structured) POST request -
+    res.redirect("/campgrounds"); //responseObject.redirect("indexPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds
+    //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
+    //responseObject.redirect("indexPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
+    //thus ending request-response cycle
+    //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -8,28 +8,26 @@ const router = express.Router(); //functionObject.method() //routerObject
 const catchAsync = require("../utils/catchAsync"); //functionObject //self create modeul/file needs "./" //going back a directory ..
 const UserClassObject = require("../models/user"); //UserClassObject(ie Model) //self created module/file needs "./" //going back a directory ..
 const passport = require("passport"); //passportObject //passport module
+const usersControllerObject = require("../controllers/users"); //exportObject //exportObject.method is named (async)handlerMiddlewareCallback //self created module/file needs "./" //going back a directory ..
 
+//*******************
+//USERS ROUTES
+//*******************
 //route1
 //httpMethod=GET,path/resource- (pathPrefixString) + /register  -(direct match/exact path)
 //(READ) name-new,purpose-display form to submit new document into (users)collection of (yelp-camp-db)db
-//router.method(pathString ,handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
+//router.method(pathString ,named handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
 //execute handlerMiddlwareCallback if (http structured) GET request arrives at path (pathPrefixString) + /register
 //arguments passed in to handlerMiddlewareCallback -
 //-if not already converted convert (http structured) request to req jsObject
 //-if not already created create res jsObject
 //-nextCallback
-router.get("/register", (req, res) => {
-  res.render("users/register");
-  //render(ejs filePath) - executes js - converts  ejs file into pure html
-  //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
-  //responseObject.render() - converts and sends res jsObject as (http structure)response //content-type:text/html
-  //thus ending request-response cycle
-});
+router.get("/register", usersControllerObject.renderRegisterForm);
 
 //route2
 //httpMethod=POST,path/resource- (pathPrefixString) + /register  -(direct match/exact path)
 //(CREATE) name-create,purpose-create new document in (users)collection of (yelp-camp-db)db
-//router.method(pathString ,createMiddlewareCallback(async handlerMiddlewareCallback)) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
+//router.method(pathString ,createMiddlewareCallback(named async handlerMiddlewareCallback)) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
 //execute handlerMiddlwareCallback if (http structured) POST request arrives at path (pathPrefixString) + /
 //arguments passed in to handlerMiddlewareCallback -
 //-already converted (http structured) request to req jsObject - (http structured) request body contained form data,previous middlewareCallback parsed it to req.body
@@ -37,82 +35,23 @@ router.get("/register", (req, res) => {
 //-nextCallback
 //async(ie continues running outside code if it hits an await inside) handlerMiddlwareCallback implicit returns promiseObject(resolved,undefined) - can await a promiseObject inside
 //async function expression without an await is just a normal syncronous function expression
-router.post(
-  "/register",
-  catchAsync(async (req, res) => {
-    try {
-      //object keys to variable - Object destructuring
-      const { email, username, password } = req.body; //form data/req.body is jsObject //{key/name:inputValue,key/name:inputValue}}
-      // ***************************************************************************************
-      //CREATE - creating a single new document in the (users) collection of (yelp-camp-db)db
-      // ***************************************************************************************
-      //modelClass
-      //UserClassObject(objectArgument-passed to constructor method)
-      //objectArgument- jsObject{key:value} ie the new document that abides to collectionSchemaInstanceObject
-      //objectArgument has validations/contraints set by collectionSchemaInstanceObject
-      //validations/contraints -
-      //email cannot be empty + other validations/constraints set by passport-local-mongoose for its fields
-      //create modelInstanceObject(ie document) - with new keyword and UserClassObject constructor method
-      const newUser = new UserClassObject({ email: email, username: username });
-      //UserClassObject.customStaticMethod(newUser,"passwordString")
-      //customStaticMethod on UserClassObject - async register()
-      //creates salt property on newUser and adds random saltValue
-      //creates hash property on newUser and adds hashValue - hashValue created by adding random saltValue to passwordString then hashing SaltedPasswordString using Pbkdf2 hashFunction
-      //calls async mongoosemMethod save on newUser - creating users collection in yelp-camp-db and or adding newUser document into users collection - can implicitly throw new Error("messageFromMongoose") - if validation/contraints broken
-      //if async mongooseMethod implicity throws Error("messageFromMongoose") inside async customStaticMethod , it catches that errorInstanceObject and throws its own new Error("messageFromPassportLocalMongoose")
-      //async customStaticMethod returns the savedUser
-      const savedUser = await UserClassObject.register(newUser, password); //savedUser = dataObject ie created jsObject(document)
-      //reqObject.passportObjectMethod(savedUser,callback) //returns promiseObject
-      //passportObjectMethod execution adds the req.user to the current sessionObject + clears previous existing current sessionObject properties - could set keepSessionInfo:true in req.login() to prevent this
-      //ie.passportObjectMethod serializes the savedUser into one value and stores it into temporary data store , making savedUser retrivable through deserializing the one value into req.user
-      //it executes the callback with parameter errorInstanceObject if it occured ,else execute callback with empty parameter
-      //callback sets success flash message and redirects to /campgrounds
-      req.login(savedUser, (err) => {
-        if (err) return next(err); //return to not run rest of code - next(e) passes erroInstanceObject to next customErrorHandlerMiddlewareCallback
-        req.flash("success", "Welcome to Yelp Camp!"); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
-        //fix for page refresh sending duplicate (http structured) POST request -
-        res.redirect("/campgrounds");
-        //responseObject.redirect("indexPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds
-        //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
-        //responseObject.redirect("indexPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
-        //thus ending request-response cycle
-        //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds
-      });
-    } catch (e) {
-      //(errorInstanceObject)
-      //we catch the implicitly thrown new Error("messageFromPassportLocalMongoose") early - closest catch - therefore .catch does not run - we dont pass it in next(e) to customErrorHandlerMiddlewareCallback
-      req.flash("error", e.message); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
-      res.redirect("/register");
-      //responseObject.redirect("registerPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /register
-      //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
-      //responseObject.redirect("registerPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
-      //thus ending request-response cycle
-      //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/register
-    }
-  })
-);
+router.post("/register", catchAsync(usersControllerObject.createUser));
 
 //route3
 //httpMethod=GET,path/resource- (pathPrefixString) + /login  -(direct match/exact path)
 //(READ) name-check,purpose-display form to submit username and password into /login post route
-//router.method(pathString ,handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
+//router.method(pathString ,named handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
 //execute handlerMiddlwareCallback if (http structured) GET request arrives at path (pathPrefixString) + /login
 //arguments passed in to handlerMiddlewareCallback -
 //-if not already converted convert (http structured) request to req jsObject
 //-if not already created create res jsObject
 //-nextCallback
-router.get("/login", (req, res) => {
-  res.render("users/login");
-  //render(ejs filePath) - executes js - converts  ejs file into pure html
-  //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
-  //responseObject.render() - converts and sends res jsObject as (http structure)response //content-type:text/html
-  //thus ending request-response cycle
-});
+router.get("/login", usersControllerObject.renderLoginForm);
 
 //route4
 //httpMethod=POST,path/resource- /login  -(direct match/exact path)
 //(CREATE) name-compare,purpose-compare and serialize existing document in (users)collection of (yelp-camp-db)db
-//router.method(pathString ,async customAuthenticationMiddlewareCallback, handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
+//router.method(pathString ,async customAuthenticationMiddlewareCallback, named handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
 //execute handlerMiddlwareCallback if (http structured) POST request arrives at path /login
 //arguments passed in to handlerMiddlewareCallback -
 //-already converted (http structured) request to req jsObject - (http structured) request body contained form data,previous middlewareCallback parsed it to req.body
@@ -125,22 +64,7 @@ router.post(
     failureRedirect: "/login",
     keepSessionInfo: true,
   }),
-  (req, res) => {
-    req.flash("success", "Welcome back!"); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
-    //req.session.returnUrl retrives urlStringObject from returnUrl property of current sessionObject - requires keepSessionInfo:true in optionsObject - else req.login() implicity execution clears previous existing current sessionObject properties
-    //req.session.returnUrl is undefined if directly coming from /login GET form route
-    //undefined(falsy) or stringObject(truthy) / urlStringObject(truthy) or stringObject(dont check)
-    const returnUrl = req.session.returnUrl || "/campgrounds";
-    //deletes the returnUrl property of current sessionObject
-    delete req.session.returnUrl;
-    //fix for page refresh sending duplicate (http structured) POST request -
-    res.redirect(returnUrl);
-    //responseObject.redirect("indexPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds
-    //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
-    //responseObject.redirect("indexPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
-    //thus ending request-response cycle
-    //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds
-  }
+  usersControllerObject.authenticatedUserRedirect
 );
 //not exact - but basic idea
 //passportObject.method(authenticationStrategyNameString,optionsObject)
@@ -176,31 +100,13 @@ router.post(
 //route5
 //httpMethod=POST,path/resource- /logout  -(direct match/exact path)
 //(CREATE) name-clear,purpose- clear req.user is current sessionObject
-//router.method(pathString ,handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
+//router.method(pathString ,named handlerMiddlewareCallback) lets us execute handlerMiddlewareCallback on specifid http method/every (http structured) request to specified path/resource
 //execute handlerMiddlwareCallback if (http structured) POST request arrives at path /logout
 //arguments passed in to handlerMiddlewareCallback -
 //-if not already converted convert (http structured) request to req jsObject
 //-if not already created create res jsObject
 //-nextCallback
-router.post("/logout", (req, res) => {
-  //reqObject.passportObjectMethod(callback)
-  //passportObjectMethod execution removes the req.user from the current sessionObject + clears previous existing current sessionObject properties - could set keepSessionInfo:true in req.logout() to prevent this
-  //ie.foundUser is retrivable through deserializing the one value from temporary data store into req.user,meaning foundUser was serialized into one value and stored into temporary data store after verifyCallback passed in customAuthenticationMiddlewareCallback at one point
-  //it executes the callback with parameter errorInstanceObject if it occured else execute callback with empty parameter
-  //callback sets success flash message and redirects to /campgrounds
-  req.logout(function (err) {
-    if (err) {
-      return next(err); //return to not run rest of code - next(e) passes erroInstanceObject to next customErrorHandlerMiddlewareCallback
-    }
-    req.flash("success", "Successfully logged out"); //stores the "messageValue" in an arrayObject in the flash property of current sessoinObject under the key "categoryKey"
-    //fix for page refresh sending duplicate (http structured) POST request -
-    res.redirect("/campgrounds"); //responseObject.redirect("indexPath") updates res.header, sets res.statusCode to 302-found ie-redirect ,sets res.location to /campgrounds
-    //resObjects header contains signed cookie created/set by express-sessions middlewareCallback
-    //responseObject.redirect("indexPath") - converts and sends res jsObject as (http structure)response // default content-type:text/html
-    //thus ending request-response cycle
-    //browser sees (http structured) response with headers and makes a (http structured) GET request to location ie default(get)/campgrounds
-  });
-});
+router.post("/logout", usersControllerObject.logoutUser);
 
 //exportsObject = custom routerObject
 module.exports = router;
