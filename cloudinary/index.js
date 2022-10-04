714c08bb9919a7902b4cb58e8b9a9f242cbd34bc
Ankit Shaji <ankitshaji@gmail.com>
2022-10-04 17:31:13 +1300

    cloudinarySdk comments rewrite

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -1,21 +1,21 @@
 //main file that gets passed in other npm package modules or user created modules
 //user created module file - can contain functionObjects,variable,classObjects etc which we can export
 
-const cloudinary = require("cloudinary").v2; //cloudinaryObject //cloudinary sdk module
+const cloudinary = require("cloudinary").v2; //cloudinaryWebApiObject //cloudinarySdk/Api module
 const { CloudinaryStorage } = require("multer-storage-cloudinary"); //ClassObject //multer-storage-cloudinary module
 
-//initializing cloudinary with our credentials using cloudinary sdk library- associate cloudinary account to this cloudinaryObject instance
-//cloudinaryObject.method(optionsObject)
+//initializing cloudinaryWebApiObject with our credentials - associate cloudinary account to this cloudinaryWebApiObject instance
+//cloudinaryWebApiObject.method(optionsObject)
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+}); //initializedCloudinaryWebApiObject
 
 //initialize CloudinaryStorage using multer-storage-cloudinary helper library //NOTE - multer library uses this helper library
 //cloudinaryStorageInstanceObject = new CloudinaryStorageClassObject(objectArgument-passed to constructor method)
 //objectArgument -
-// - set cloudnaryObject instance
+// - set initializedCloudinaryWebApiObject instance
 // - set folder to use on cloudinary to store images in
 // - set allowed file formats that can be stored
 const cloudinaryStorageInstanceObject = new CloudinaryStorage({
