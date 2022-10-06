4dfb6bcae87a7cc8d6176f3dd5e889c0b6a61f0d
Ankit Shaji <ankitshaji@gmail.com>
2022-10-06 16:53:10 +1300

    use initializedMapboxglObject to add popup to markerInstanceObject

--- public/js/renderShowPageMap.js
+++ public/js/renderShowPageMap.js
@@ -2,19 +2,26 @@
 //intialize mapboxglObject with our cerdentials - associate mapbox account to this mapboxglObject instance
 mapboxgl.accessToken = MAPBOX_APIS_PUBLIC_ACCESS_TOKEN; //using previously exectued client side js scripts variable
 //intializedMapboxglObject.property = MapClassObject()
-//mapInstanceObject =  new MapClassObject(objectArgument-passed to constructor method) //NOTE - container property is div id
+//mapInstanceObject =  new MapClassObject(objectArgument-passed to constructor method) //NOTE - container property is div id, can choose diffrent stlye
 const map = new mapboxgl.Map({
   container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v11", // style URL
+  style: "mapbox://styles/mapbox/light-v10", // style URL
   center: campgroundJsonStringObject.geometry.coordinates, //campgroundJsonStringObject.property.property = arrayObject// starting position [lng, lat]
   zoom: 10, // starting zoom
 });
 
-//Create a default Marker and add it to the map.
+//Create a default Marker with Popup and add it to the map.
 //intializedMapboxglObject.property = MarkerClassObject()
 //markerInstanceObject =  new MarkerClassObject(executes empty constructor method)
-//markerInstanceObject.method(lngLatArrayObject).method(mapInstanceObject)  //method chaining
+//markerInstanceObject.method(lngLatArrayObject).method(popupInstanceObject.method(htmlStringObject)).method(mapInstanceObject)  //method chaining - each method returns markerInstanceObject
 //lngLatArrayObject = campgroundJsonStringObject.property.property
+//intializedMapboxglObject.property = PopupClassObject()
+//popupInstanceObject = new PopupClassObject(objectArgument-passed to constructor method)
 const marker1 = new mapboxgl.Marker()
   .setLngLat(campgroundJsonStringObject.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campgroundJsonStringObject.title}</h3>${campgroundJsonStringObject.location}<p></p>`
+    )
+  )
   .addTo(map);
