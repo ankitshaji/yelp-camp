0e9105e722de0bb267364b9c8b143c21b3576b3d
Ankit Shaji <ankitshaji@gmail.com>
2022-10-06 22:17:40 +1300

    repopulate campgrounds collection-set geometry field value based on city

--- seeds/index.js
+++ seeds/index.js
@@ -82,7 +82,14 @@ const seedDb = async () => {
         title: `${sample(descriptors)} ${sample(places)}`,
         //arrayOfObjects[randomNumberIndex].property
         location: `${cities[random1000].city}, ${cities[random1000].state}`,
-        geometry: { type: "Point", coordinates: [-113.1331, 47.0202] },
+        //[arrayObject[randomNumberIndex].property,arrayObject[randomNumberIndex].property]
+        geometry: {
+          type: "Point",
+          coordinates: [
+            cities[random1000].longitude,
+            cities[random1000].latitude,
+          ],
+        },
         images: [
           {
             url: "https://res.cloudinary.com/dh9ncm8mp/image/upload/v1664596956/YelpCamp/sq26zqx5ac2o1g8eokts.jpg",
